01c839413c5898205da3334ca38db8a44a866a42
nurih <1554735+nurih@users.noreply.github.com>
2019-04-02 13:57:41 -0700

    Echo not-understood utterance

--- bot.js
+++ bot.js
@@ -1,15 +1,20 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-// Import required Bot Framework classes.
-const { ActivityTypes, MessageFactory, TurnContext } = require('botbuilder');
-const { CardFactory } = require('botbuilder');
+const {
+    ActivityTypes,
+    MessageFactory,
+    TurnContext
+} = require('botbuilder');
+const {
+    CardFactory
+} = require('botbuilder');
 
-// Adaptive Card content
 const IntroCard = require('./resources/IntroCard.json');
 
-// Welcomed User property name
 const WELCOMED_USER = 'welcomedUserProperty';
+
+const ValidExplorationOptions = ['FAQS', 'Band Search', 'Navigate'];
 
 class WelcomeBot {
     /**
@@ -18,8 +23,6 @@ class WelcomeBot {
      *                    if the bot had already welcomed the user
      */
     constructor(userState) {
-        // Creates a new user property accessor.
-        // See https://aka.ms/about-bot-state-accessors to learn more about the bot state and state accessors.
         this.welcomedUserProperty = userState.createProperty(WELCOMED_USER);
 
         this.userState = userState;
@@ -37,54 +40,30 @@ class WelcomeBot {
                 await this.welcomedUserProperty.set(turnContext, true);
             } else {
                 let text = turnContext.activity.text;
-                const validOptions = ['FAQS', 'Band Search', 'Navigate'];
-                if (validOptions.includes(text)) {
+                if (this.isValidChoice(text)) {
                     await turnContext.sendActivity(`You clicked ${ text }! `);
-                    await this.sendSuggestedActions(turnContext);
                 } else {
-                    await turnContext.sendActivity('U R WRONG');
-                    await this.sendSuggestedActions(turnContext);
+                    await turnContext.sendActivity(`Sorry. You said ${ text } but I don't understand what you want.`);
                 }
+                await this.sendSuggestedActions(turnContext);
             }
+        } else {
+            console.log(turnContext.activity.type);
         }
         // Save state changes
         await this.userState.saveChanges(turnContext);
     }
 
-    /**
-     * Sends welcome messages to conversation members when they join the conversation.
-     * Messages are only sent to conversation members who aren't the bot.
-     * @param {TurnContext} turnContext
-     */
-    async sendWelcomeMessage(turnContext) {
-        // Do we have any new members added to the conversation?
-        if (turnContext.activity.membersAdded.length !== 0) {
-            // Iterate over all new members added to the conversation
-            for (let idx in turnContext.activity.membersAdded) {
-                // Greet anyone that was not the target (recipient) of this message.
-                // Since the bot is the recipient for events from the channel,
-                // context.activity.membersAdded === context.activity.recipient.Id indicates the
-                // bot was added to the conversation, and the opposite indicates this is a user.
-                if (turnContext.activity.membersAdded[idx].id !== turnContext.activity.recipient.id) {
-                    await turnContext.sendActivity(`Welcome to the 'Welcome User' Bot. This bot will introduce you to welcoming and greeting users.`);
-                    await turnContext.sendActivity("You are seeing this message because the bot received at least one 'ConversationUpdate'" +
-                        'event,indicating you (and possibly others) joined the conversation. If you are using the emulator, ' +
-                        "pressing the 'Start Over' button to trigger this event again. The specifics of the 'ConversationUpdate' " +
-                        'event depends on the channel. You can read more information at https://aka.ms/about-botframework-welcome-user');
-                    await turnContext.sendActivity(`It is a good pattern to use this event to send general greeting to user, explaining what your bot can do. ` +
-                        `In this example, the bot handles 'hello', 'hi', 'help' and 'intro. ` +
-                        `Try it now, type 'hi'`);
-                }
-            }
-        }
+    isValidChoice(text) {
+        return ValidExplorationOptions.includes(text);
     }
 
     /**
- * Send suggested actions to the user.
- * @param {TurnContext} turnContext A TurnContext instance containing all the data needed for processing this conversation turn.
- */
+     * Send suggested actions to the user.
+     * @param {TurnContext} turnContext A TurnContext instance containing all the data needed for processing this conversation turn.
+     */
     async sendSuggestedActions(turnContext) {
-        var reply = MessageFactory.suggestedActions(['FAQs', 'Band Search', 'Navigate'], 'How would you like to explore the event?');
+        var reply = MessageFactory.suggestedActions(ValidExplorationOptions, 'How would you like to explore the event?');
         await turnContext.sendActivity(reply);
     }
 }
