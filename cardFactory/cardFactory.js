9cc4ccc45b8627638f7e02f5879b384b2342a3b3
Ryan Ferguson <ryan_ferguson@mckinsey.com>
2019-04-03 16:58:43 -0700

    fixes

--- cardFactory/cardFactory.js
+++ cardFactory/cardFactory.js
@@ -3,18 +3,10 @@ const {
   CardFactory
 } = require('botbuilder');
 
-function createHeroCard() {
-  return CardFactory.heroCard(
-    'BotFramework Hero Card',
-    CardFactory.images(['https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg']),
-    CardFactory.actions([
-      {
-        type: 'openUrl',
-        title: 'Get started',
-        value: 'https://docs.microsoft.com/en-us/azure/bot-service/'
-      }
-    ])
-  );
+function createHeroCard(choice) {
+  console.log(choice)
+  return CardFactory.heroCard(choice.bandName, [`https://westeuropebotassets.blob.core.windows.net/assets/images/${choice.image}`],
+    '', { text: choice.description });
 }
 
 module.exports.createHeroCard = createHeroCard;
